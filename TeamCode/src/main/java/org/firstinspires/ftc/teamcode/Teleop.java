42fb8e3dd94ce39710fffb043becf6697f2cd61b
Programmer2 <drss.ftc.12653@gmail.com>
2022-09-23 19:11:58 -0400

    Added: Speed controls Gamepad controls Drive functions ~Micah Lander

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+/*
+@TeleOp(name = "FY22TeleOp", group = "TeleOp" )
+public class Teleop extends LinearOpMode {
+    DcMotor topRight;
+    DcMotor bottomRight;
+    DcMotor topLeft;
+    DcMotor bottomLeft;
+
+    double speed = 1;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //hardware maps
+        topRight = hardwareMap.dcMotor.get("TR"); // control hub port 0
+        bottomRight = hardwareMap.dcMotor.get("BR"); //control hub port 1
+        topLeft = hardwareMap.dcMotor.get("TL"); //control hub port 2
+        bottomLeft = hardwareMap.dcMotor.get("BL"); //control hub port 3
+    }
+
+    //Set gamepad
+    float gamepad1LeftY = -gamepad1.left_stick_x; //Sets the gamepads left sticks y position to a float
+    float gamepad1LeftX = gamepad1.left_stick_y; //Sets the gameepads left sticks x position to a float
+    float gamepad1RightX = gamepad1.right_stick_x; //Sets the gamepads right sticks x position to a float
+    float gamepad2RightY = gamepad2.right_stick_y; // Sets the 2nd gamepads right sticks x position to a float;
+
+    //Mechenum formulas
+    double TopRightSpeed = gamepad1LeftY +gamepad1LeftX + gamepad1RightX; //Combines the inputs of the sticks to clip their output to a value between 1 and -1
+    double TopLeftSpeed = -gamepad1LeftY + gamepad1LeftX + gamepad1RightX; //Combines the inputs of the sticks to clip their output to a value between 1 and -1
+    double BottomRightSpeed = gamepad1LeftY - gamepad1LeftX + gamepad1RightX; //Combines the inputs of the sticks to clip their output to a value between 1 and -1
+    double BottomLeftSpeed = -gamepad1LeftY - gamepad1LeftX + gamepad1RightX; //Combines the inputs of the sticks to clip their output to a value between 1 and -1
+
+    // sets speed
+    double topLeftCorrectedSpeed = Range.clip(Math.pow(TopRightSpeed, 3), -speed, speed); //Slows down the motor and sets its max/min speed to the double "speed"
+    double topRightCorrectedSpeed = Range.clip(Math.pow(TopLeftSpeed, 3), -speed, speed); //Slows down the motor and sets its max/min speed to the double "speed"
+    double bottomLeftCorrectedSpeed = Range.clip(Math.pow(BottomRightSpeed, 3), -speed, speed); //Slows down the motor and sets its max/min speed to the double "speed"
+    double bottomRightCorrectedSpeed = Range.clip(Math.pow(BottomLeftSpeed, 3), -speed, speed); //Slows down the motor and sets its max/min speed to the double "speed"
+
+    topRight.setPower(topRightCorrectedSpeed);
+    bottomRight.setPower(bottomRightCorrectedSpeed);
+    topLeft.setPower(topLeftCorrectedSpeed);
+    bottomLeft.setPower(bottomLeftCorrectedSpeed);
+}
+
+*/
